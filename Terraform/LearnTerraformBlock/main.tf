0bd55b701179fbf4fcacd8aaafca259c0fcf47bd
morashish24 <morashish24@gmail.com>
2026-03-23 00:59:45 +0530

    commit

--- Terraform/LearnTerraformBlock/main.tf
+++ Terraform/LearnTerraformBlock/main.tf
@@ -58,10 +58,8 @@ resource "aws_s3_bucket" "my_s3_bucket" {
 output "bucket_name" {
   value = aws_s3_bucket.my_s3_bucket.bucket
 }
-*/
-output "ec2_public_ip" {
 
-  count = 3
+output "ec2_public_ip" {
   value =  aws_instance.my_ec2_instance[count.index]
 }
-
+*/
