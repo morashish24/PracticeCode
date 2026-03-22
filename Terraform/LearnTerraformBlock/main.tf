a4e24d398b10b630bdde4e3348aebab8781f7df7
morashish24 <morashish24@gmail.com>
2026-03-22 18:33:49 +0530

    push

--- Terraform/LearnTerraformBlock/main.tf
+++ Terraform/LearnTerraformBlock/main.tf
@@ -1,31 +1,63 @@
 terraform {
-    required_providers = "aws"
-    version = "~>3.4"
-    backend {
-        s3 
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.4"
     }
-    dynammo_db = 
-    profile = "default"   # Its optional. Profile is written to choose the aws credentials which need to be used stored in .aws/credentials file
+  }
+
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"   # change this (must be unique & already created)
+    key            = "ec2/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"              # change if needed (must exist)
+    profile        = "default"
+  }
 }
 
 provider "aws" {
-    version = 
-    region = "us-east-1"
-    profile = "default"
+  region  = "us-east-1"
+  profile = "default"
 }
 
-resources "ec2_instance" "my_ec2_instance" {
-    type = "t2.micro"
-    image = ""
-  
+# Get latest Amazon Linux 2 AMI dynamically
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
 }
 
-resource "aws_s3_bucket" "my-s3-bucket" {
-    bucket = "my-unique-s3-bucket"
-    acl    = "private"
+# EC2 Instance
+resource "aws_instance" "my_ec2_instance" {
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.amazon_linux.id
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
 }
 
+# S3 Bucket
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "my-unique-s3-bucket-12345"  # MUST be globally unique
+
+  tags = {
+    Name = "MyS3Bucket"
+  }
+}
+
+# Output
 output "bucket_name" {
-    value = "aws_s3_bucket.my-aws_s3_bucket.bucket"
+  value = aws_s3_bucket.my_s3_bucket.bucket
+}
 
+output "ec2_public_ip" {
+  value = aws_instance.my_ec2_instance.public_ip
 }
