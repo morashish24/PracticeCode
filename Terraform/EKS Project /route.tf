59395f00fae70c84ddd34261c85152e8ba716e7d
morashish24 <morashish24@gmail.com>
2026-03-24 02:27:06 +0530

    commit

---
+++ Terraform/EKS Project /route.tf
@@ -0,0 +1,19 @@
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+resource "aws_route" "route" {
+  route_table_id         = aws_route_table.rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "a1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "a2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.rt.id
+}
