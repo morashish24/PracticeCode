2d1c5d59fed8170b4d00f2c75b02da32cc472bc8
morashish24 <morashish24@gmail.com>
2026-03-24 01:37:35 +0530

    commit

--- Terraform/EKS Project /vpc.tf
+++ Terraform/EKS Project /vpc.tf
@@ -5,3 +5,17 @@ resource "aws_vpc" "eks_vpc" {
     Name = "eks-vpc"
   }
 }
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
