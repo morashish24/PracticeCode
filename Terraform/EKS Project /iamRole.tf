415d1231e9e1417e176f6c03827d6ed90439a1be
morashish24 <morashish24@gmail.com>
2026-03-26 00:30:38 +0530

    commit

--- Terraform/EKS Project /iamRole.tf
+++ Terraform/EKS Project /iamRole.tf
@@ -12,3 +12,8 @@ resource "aws_iam_role" "eks_cluster_role" {
     }]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "eks_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
