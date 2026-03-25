77fc5b49a81fcc2c11672e5fb2bb128d5a4c4c8b
morashish24 <morashish24@gmail.com>
2026-03-26 00:58:36 +0530

    commit

--- Terraform/EKS Project /eksCluster.tf
+++ Terraform/EKS Project /eksCluster.tf
@@ -1,5 +1,10 @@
 resource "aws_eks_cluster" "eks" {
   name     = "my-eks-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
-
+    vpc_config {
+        subnet_ids = [
+        aws_subnet.public_subnet_1.id,
+        aws_subnet.public_subnet_2.id
+        ]
+    }
 }
