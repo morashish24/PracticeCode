7ea3d6814e653ecccfa03c763f0fde450a75ac64
morashish24 <morashish24@gmail.com>
2026-03-26 01:07:25 +0530

    commit

--- Terraform/EKS Project /iamRole.tf
+++ Terraform/EKS Project /iamRole.tf
@@ -17,3 +17,18 @@ resource "aws_iam_role_policy_attachment" "eks_policy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+
+resource "aws_iam_role" "node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
