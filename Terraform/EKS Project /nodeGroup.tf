df55d776576da1b33136890ba676e4eae66285fc
morashish24 <morashish24@gmail.com>
2026-03-26 01:14:29 +0530

    commit

---
+++ Terraform/EKS Project /nodeGroup.tf
@@ -0,0 +1,18 @@
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "my-node-group"
+  node_role_arn   = aws_iam_role.node_role.arn
+
+  subnet_ids = [
+    aws_subnet.public_subnet_1.id,
+    aws_subnet.public_subnet_2.id
+  ]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t2.micro"]
+}
