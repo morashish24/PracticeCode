43c8e21eb47d1f5e4cca5e1998b2c5f60b977293
morashish24 <morashish24@gmail.com>
2026-03-27 01:40:59 +0530

    commit

---
+++ Terraform/EKS Project /ec2.tf
@@ -0,0 +1,88 @@
+# Get latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# Security Group
+resource "aws_security_group" "jenkins_sg" {
+  name = "jenkins-sg"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Jenkins"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Docker (optional)"
+    from_port   = 2375
+    to_port     = 2375
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "jenkins_server" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"  # Free tier
+
+  security_groups = [aws_security_group.jenkins_sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+
+              # Install Java (required for Jenkins)
+              amazon-linux-extras install java-openjdk11 -y
+
+              # Install Jenkins
+              wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+              rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+              yum install jenkins -y
+
+              # Install Docker
+              yum install docker -y
+              systemctl start docker
+              systemctl enable docker
+
+              # Add ec2-user to docker group
+              usermod -aG docker ec2-user
+
+              # Start Jenkins
+              systemctl start jenkins
+              systemctl enable jenkins
+              EOF
+
+  tags = {
+    Name = "Jenkins-Server"
+  }
+}
+
+# Output Public IP
+output "instance_public_ip" {
+  value = aws_instance.jenkins_server.public_ip
+}
