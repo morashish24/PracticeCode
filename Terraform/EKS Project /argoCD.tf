db737933720dfef3f7bba6c5dbf1d89dd9d2ccc5
morashish24 <morashish24@gmail.com>
2026-03-30 01:54:46 +0530

    c

--- Terraform/EKS Project /argoCD.tf
+++ Terraform/EKS Project /argoCD.tf
@@ -28,3 +28,20 @@ resource "helm_release" "argocd" {
   }
   ]
 }
+
+data "aws_eks_cluster" "eks" {
+  name = "my-eks-cluster"
+}
+data "aws_eks_cluster_auth" "eks" {
+  name = "my-eks-cluster"
+}
+
+provider "kubernetes" {
+  host = aws_eks_cluster.eks.endpoint
+
+  cluster_ca_certificate = base64decode(
+    aws_eks_cluster.eks.certificate_authority[0].data
+  )
+
+  token = data.aws_eks_cluster_auth.eks.token
+}
