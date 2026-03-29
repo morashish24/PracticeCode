38edf71f47eca05c5bd1fa0b2c827796ccbae756
morashish24 <morashish24@gmail.com>
2026-03-29 17:48:09 +0530

    co

--- Terraform/EKS Project /argoCD.tf
+++ Terraform/EKS Project /argoCD.tf
@@ -5,7 +5,7 @@ resource "kubernetes_namespace_v1" "argocd" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = data.aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(
       data.aws_eks_cluster.eks.certificate_authority[0].data
