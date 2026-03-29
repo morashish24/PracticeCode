ee2ec90d05b8a098bbb02a1e45f79a9f8402eb43
morashish24 <morashish24@gmail.com>
2026-03-29 17:36:38 +0530

    commit

--- Terraform/EKS Project /eksCluster.tf
+++ Terraform/EKS Project /eksCluster.tf
@@ -8,3 +8,19 @@ resource "aws_eks_cluster" "eks" {
         ]
     }
 }
+
+data "aws_eks_cluster" "eks" {
+  name = "my-eks-cluster"
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = "my-eks-cluster"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(
+    data.aws_eks_cluster.eks.certificate_authority[0].data
+  )
+  token = data.aws_eks_cluster_auth.eks.token
+}
